e2fb5c11dc74704f2508f85332329ce0fb13939a
Igor Couto <igor.fcouto@gmail.com>
2026-05-30 14:37:26 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
